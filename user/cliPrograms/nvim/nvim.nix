a409ac7693e25187f832f41f755192687caa0693
nintharray <167829823+nintharray@users.noreply.github.com>
2024-05-09 13:11:11 -0400

    add options.lua

--- user/cliPrograms/nvim/nvim.nix
+++ user/cliPrograms/nvim/nvim.nix
@@ -3,5 +3,13 @@
 {
     programs.neovim = {
     	enable = true;
+
+	viAlias = true;
+	vimAlias = true;
+	vimdiffAlias = true;
+
+	extraLuaConfig = ''
+	    ${builtins.readFile ./options.lua}
+	'';
     };
 }
