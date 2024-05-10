4fe65db60bfe9956d5bb2bb42b445a2b13426316
nintharray <167829823+nintharray@users.noreply.github.com>
2024-05-10 13:25:06 -0400

    change git default branch name to main

--- profiles/homelab/home.nix
+++ profiles/homelab/home.nix
@@ -78,6 +78,9 @@ in
 		enable = true;
 		userName = "nintharray";
 		userEmail = "167829823+nintharray@users.noreply.github.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
 	};
 
 	programs.bash = {
