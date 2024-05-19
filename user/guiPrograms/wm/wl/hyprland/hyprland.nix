1464a62f6940c1a2d97388e01a8e49156ded78c2
nintharray <167829823+nintharray@users.noreply.github.com>
2024-05-19 03:18:48 -0400

    create initial waybar config (based off gvolpe/nix-config)

--- user/guiPrograms/wm/wl/hyprland/hyprland.nix
+++ user/guiPrograms/wm/wl/hyprland/hyprland.nix
@@ -2,37 +2,58 @@
 
 {
   imports = [
+    ../waybar/waybar.nix
     ../../gtk-qt.nix
+  ];
+
+  home.packages = with pkgs; [
+    paprefs
+    pasystray
+    pavucontrol
+    playerctl
+    pulsemixer
+    brightnessctl
+    loupe
+    grim
+    grimblast
+    libnotify
+    wl-clipboard
+    wofi
+    xwaylandvideobridge
   ];
 
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
-    "$mod" = "SUPER";
-    "$terminal" = "kitty";
-    bind =
-      [
-        "$mod, F, exec, firefox"
-        # ", Print, exec, grimblast copy area"
-        "$mod, Return, exec, $terminal"
-        "$mod SHIFT, E, exit"
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-        builtins.concatLists (builtins.genList (
-            x: let
-              ws = let
-                c = (x + 1) / 10;
-              in
-                builtins.toString (x + 1 - (c * 10));
-            in [
-              "$mod, ${ws}, workspace, ${toString (x + 1)}"
-              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-            ]
-          )
-          10)
-      );
+      "$mod" = "SUPER";
+      "$terminal" = "kitty";
+      bind =
+        [
+          "$mod, F, exec, firefox"
+          # ", Print, exec, grimblast copy area"
+          "$mod, Return, exec, $terminal"
+          "$mod SHIFT, E, exit"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+          builtins.concatLists (builtins.genList (
+              x: let
+                ws = let
+                  c = (x + 1) / 10;
+                in
+                  builtins.toString (x + 1 - (c * 10));
+              in [
+                "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              ]
+            )
+            10)
+        );
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+      };
       decoration = {
         drop_shadow = false;
         blur.enabled = false;
@@ -47,5 +68,8 @@
         vfr = true;
       };
     };
+    extraConfig = ''
+      exec-once=${pkgs.waybar}/bin/waybar
+    '';
   };
 }
