a8bdf17a70f3dde96524d8405e429c29f2991993
nintharray <167829823+nintharray@users.noreply.github.com>
2024-05-19 03:18:07 -0400

    add plenty of logo fonts

--- profiles/t430/configuration.nix
+++ profiles/t430/configuration.nix
@@ -51,6 +51,12 @@
 	neovim
 	];
 
+  fonts.packages = with pkgs; [
+    font-awesome
+    material-design-icons
+    nerdfonts
+  ];
+
 	# Some programs need SUID wrappers, can be configured further or are
 	# started in user sessions.
 	# programs.mtr.enable = true;
