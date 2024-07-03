ca34ef5d4627867f199237c39e2e4c8f1c8032b5
nintharray <167829823+nintharray@users.noreply.github.com>
2024-07-02 23:36:59 -0400

    feat: add BOSL OpenSCAD Library

--- user/guiPrograms/3d/cad.nix
+++ user/guiPrograms/3d/cad.nix
@@ -5,6 +5,13 @@
     openscad
     kicad
   ];
+  home.file.".local/share/OpenSCAD/libraries/BOSL".source =
+      pkgs.fetchFromGitHub {
+        owner = "revarbat";
+        repo = "BOSL";
+        rev = "refs/heads/master";
+        sha256 = "1s08jx4npiqyryh7h3kcr7hb2razqg3kz4sm5cyysg8kvldfm2yv";
+      };
   home.file.".local/share/OpenSCAD/libraries/BOSL2".source =
       pkgs.fetchFromGitHub {
         owner = "revarbat";
